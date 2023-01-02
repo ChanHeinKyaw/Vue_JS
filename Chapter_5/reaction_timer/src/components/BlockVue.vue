df0ab0c0f9a424ab1acbf3b9bba39d537449a00f
ChanHeinKyaw <scm.chanheinkyaw@gmail.com>
2023-01-02 15:38:58 +0630

    get user click reaction score

--- Chapter_5/reaction_timer/src/components/BlockVue.vue
+++ Chapter_5/reaction_timer/src/components/BlockVue.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="block" v-if="showBlock">Click Here {{ delay }}</div>
+  <div class="block" v-if="showBlock" @click="stopTimer">
+    Click Here {{ delay }}
+  </div>
 </template>
 
 <script>
@@ -8,12 +10,25 @@ export default {
   data() {
     return {
       showBlock: false,
+      score: 0,
+      timer: null,
     };
   },
   mounted() {
     setTimeout(() => {
       this.showBlock = true;
+      this.startTimer();
     }, this.delay);
+  },
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.score += 50;
+      }, 50);
+    },
+    stopTimer() {
+      clearInterval(this.timer);
+    },
   },
 };
 </script>
