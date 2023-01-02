77e175b8639712e71d297e26ef609ad7fd326c3d
ChanHeinKyaw <scm.chanheinkyaw@gmail.com>
2023-01-02 17:02:02 +0630

    show dynamic game result

---
+++ Chapter_5/reaction_timer/src/components/ResultVue.vue
@@ -0,0 +1,18 @@
+<template>
+  <p>Result is {{ score }}ms</p>
+  <div v-if="score < 250">
+    <p>you are too fase</p>
+  </div>
+  <div v-else-if="score > 250 && score < 500">
+    <p>you are normal</p>
+  </div>
+  <div v-else>
+    <p>you are too slow! try again</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["score"],
+};
+</script>
