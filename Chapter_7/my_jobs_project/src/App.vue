17b342019d9a4c107bb387d81e2f48fd9af03bdf
ChanHeinKyaw <scm.chanheinkyaw@gmail.com>
2023-01-09 15:11:50 +0630

    router link and create our first route

--- Chapter_7/my_jobs_project/src/App.vue
+++ Chapter_7/my_jobs_project/src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link :to="{ name: 'home' }">Home</router-link> |
+    <router-link :to="{ name: 'about' }">About</router-link> |
+    <router-link :to="{ name: 'jobs' }">Job</router-link> |
+    <router-link :to="{ name: 'jobdetail' }">JobDetail</router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
---
+++ Chapter_7/my_jobs_project/src/views/jobs/JobDetail.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>This is and Job Detail Page</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
