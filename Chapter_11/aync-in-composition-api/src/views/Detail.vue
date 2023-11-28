c0a87544506f508a9b8f435617956dbb433f130e
ChanHeinKyaw <chanheinkyaw.mm@gmail.com>
2023-11-28 20:00:34 +0630

    post detail page and getPost composable

---
+++ Chapter_11/aync-in-composition-api/src/views/Detail.vue
@@ -0,0 +1,29 @@
+<template>
+    <div v-if="error">
+      {{ error }}
+    </div>
+    <h1>Post Detail</h1>
+    <div v-if="post">
+        {{ post }}
+    </div>
+    <div v-else>
+        loading....
+    </div>
+</template>
+
+<script>
+import getPost from "../composables/getPost";
+export default {
+    props: ['id'],
+    setup (props){
+        let {post,error,load} = getPost(props.id);
+
+        load();
+        
+        return {
+            post,
+            error
+        }
+    }
+}
+</script>
