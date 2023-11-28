f4781edb2b259f3490a17bca3e49fd5e43e890b1
ChanHeinKyaw <chanheinkyaw.mm@gmail.com>
2023-11-28 18:00:11 +0630

    lifecycle hook in composition api

--- Chapter_10/composition-api/src/views/HomeView.vue
+++ Chapter_10/composition-api/src/views/HomeView.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="home">
     <h1>Post Lists</h1>
-    <PostList :posts="posts"></PostList>
+    <div v-if="showPost">
+      <PostList :posts="posts"></PostList>
+    </div>
+    <button @click="showPost=!showPost">Toggle Post</button>
+    <button @click="posts.pop()">Delete</button>
   </div>
 </template>
 
@@ -11,13 +15,15 @@ import { ref } from 'vue';
 export default {
   components: { PostList },
   setup(){
+    let showPost = ref(true);
     let posts = ref([
       {title: "post title one", body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged", id: 1},
       {title: "post title two", body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged", id: 2}
     ])
 
     return {
-      posts
+      posts,
+      showPost
     }
   }
 }
