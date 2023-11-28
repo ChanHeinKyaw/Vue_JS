a055643731cf465b8b6fe382225bbe39aab4634d
ChanHeinKyaw <chanheinkyaw.mm@gmail.com>
2023-11-28 14:36:13 +0630

    using refs for reactivity

--- Chapter_10/composition-api/src/views/HomeView.vue
+++ Chapter_10/composition-api/src/views/HomeView.vue
@@ -2,23 +2,31 @@
   <div class="home">
     <h1>Home Page</h1>
     <p>Hello {{ name }}.i am {{ age }} years old</p>
+    <input type="text" v-model="name">
     <button @click="handleClick">Click</button>
+    <button @click="increaseAge">Increase</button>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue';
 export default {
   setup(){
-    let name = "Chan Hein Kyaw";
-    let age = 23;
+    let name = ref("Chan Hein Kyaw");
+    let age = ref(23);
     let handleClick = () => {
-      console.log("u clicked me");
+      name.value = "Kyaw Kyaw"
+    }
+
+    let increaseAge = () => {
+      age.value++;
     }
 
     return {
       name,
       age,
-      handleClick
+      handleClick,
+      increaseAge
     }
   }
 }
