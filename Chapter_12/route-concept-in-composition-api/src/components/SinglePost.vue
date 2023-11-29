558118dddd015ef603c9e6937972572fb2f1eda7
ChanHeinKyaw <chanheinkyaw.mm@gmail.com>
2023-11-29 09:17:34 +0630

    Chapter-15 route concept in composition api

---
+++ Chapter_12/route-concept-in-composition-api/src/components/SinglePost.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="main">
+        <div class="card">
+            <router-link :to="{name: 'detail', params: {id: post.id}}">
+            <h3>{{post.title}}</h3>
+            </router-link>
+            <p>{{cutPostBody}}</p>
+            <span v-for="tag in post.tags" :key="tag" class="pill">
+                {{ tag }}
+            </span>
+        </div>
+    </div>
+</template>
+
+<script>
+import { computed } from 'vue'
+export default {
+    props: ["post"],
+    setup(props){
+        let cutPostBody = computed(() => {
+            return props.post.body.substring(0,100)+ "....";
+        })
+
+        return {
+            cutPostBody
+        };
+    }
+}
+</script>
+
+<style scoped>
+    .card{
+        background-color: #f2f2f2;
+        padding: 10px;
+        margin-bottom: 10px;
+        border-radius: 5px;
+        width: 45%;
+    }
+    
+    .main{
+        display: flex;
+        justify-content: center;
+        text-align: left;
+    }
+</style>
---
+++ Chapter_12/route-concept-in-composition-api/src/views/Detail.vue
@@ -0,0 +1,31 @@
+<template>
+    <div v-if="error">
+      {{ error }}
+    </div>
+    <h1>Post Detail</h1>
+    <div v-if="post">
+        {{ post }}
+    </div>
+    <div v-else>
+        <Spinner></Spinner>
+    </div>
+</template>
+
+<script>
+import Spinner from '../components/Spinner'
+import getPost from "../composables/getPost";
+export default {
+  components: { Spinner },
+    props: ['id'],
+    setup (props){
+        let {post,error,load} = getPost(props.id);
+
+        load();
+        
+        return {
+            post,
+            error
+        }
+    }
+}
+</script>
