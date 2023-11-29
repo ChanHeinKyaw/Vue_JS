8e14a10cb393003c36b235a2f02b6dbb88251593
ChanHeinKyaw <chanheinkyaw.mm@gmail.com>
2023-11-29 14:14:13 +0630

    tag cloud unique links

--- Chapter_12/route-concept-in-composition-api/src/components/TagCloud.vue
+++ Chapter_12/route-concept-in-composition-api/src/components/TagCloud.vue
@@ -1,13 +1,43 @@
 <template>
-  <h1>Tag Cloud</h1>
+  <h4>Tag Cloud</h4>
+  <div>
+    <div v-for="tag in uniqueTags" :key="tag">
+      <router-link :to="{name: 'tag', params: {tag}}">
+          <p class="badge">{{ tag }}</p>
+      </router-link>
+    </div>
+  </div>
 </template>
 
 <script>
+import { ref } from 'vue';
 export default {
+  props: ['posts'],
+  setup(props){
+    let tags = ref([]);
+    props.posts.forEach(post => {
+      post.tags.forEach(tag => {
+        tags.value.push(tag);
+      })
+    });
 
+    let uniqueTags = tags.value.filter((tag,index,array) => {
+      return array.indexOf(tag) === index;
+    })
+  
+    return {
+      uniqueTags
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped>
+  .badge{
+    color: #929292;
+  }
 
+  a[href]{
+    text-decoration: none;
+  }
 </style>
