1928499845cf45bc5196afa70d48556c26236faf
ChanHeinKyaw <chanheinkyaw.mm@gmail.com>
2023-12-02 22:55:12 +0630

    change our date format with date fns library

--- Chapter_14/live-chat-room-application-project/src/components/ChatWindow.vue
+++ Chapter_14/live-chat-room-application-project/src/components/ChatWindow.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="chat-window">
-    <div class="messages" v-for="message in messages" :key="message.id">
-      <div class="single">
-        <span class="created-at">{{ message.created_at.toDate() }}</span>
+    <div class="messages">
+      <div
+        class="single"
+        v-for="message in formattedMessages"
+        :key="message.id"
+      >
+        <span class="created-at">{{ message.created_at }}</span>
         <span class="name">{{ message.name }}</span>
         <span class="message">{{ message.message }}</span>
       </div>
@@ -12,10 +16,18 @@
 
 <script>
 import { db } from "@/firebase/config";
-import { ref } from "vue";
+import { computed, ref } from "vue";
+import { formatDistanceToNow } from "date-fns";
 export default {
   setup() {
     let messages = ref([]);
+    let formattedMessages = computed(() => {
+      return messages.value.map((msg) => {
+        let formateTime = formatDistanceToNow(msg.created_at.toDate());
+        return { ...msg, created_at: formateTime };
+      });
+    });
+
     db.collection("messages")
       .orderBy("created_at")
       .onSnapshot((snap) => {
@@ -27,7 +39,7 @@ export default {
         messages.value = results;
       });
 
-    return { messages };
+    return { messages, formattedMessages };
   },
 };
 </script>
